e447f046c6834a8b9b834cd4e70464c3e2d3a144
shukurali <shukuralijob@gmail.com>
2024-04-03 12:04:42 +0500

    feat(static)

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+
+   // Crol to top
+  var $btnTop = $(".btn-top");
+  $(window).on("scroll",function(){
+    if($(window).scrollTop() >= 20)
+    {
+      $btnTop.fadeIn();
+    }
+    else{
+      $btnTop.fadeOut();
+    }
+  });
+  $btnTop.on("click",function(){
+    $("html,body").animate({scrollTop:0},1200)
+  });
+
+
+  
